f5f7618bcac0075437d8f83a0b3844161be5aa93
Mitchell Gannon <mgannon@flandersinc.com>
2024-02-09 14:54:00 +0800

    The login parameters are now being passed into the sql injection checking function

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ### TODO
 - Add in some sort of object to the main program that can hold a list of strings
-- add a text file to the ids project that has as list of all the common sql vulnerabilities 
 - read in the list from a volume that is mapped into the container
 - place those items in the list
 - when the ids is called from request handler, check that the username or password does not match anything in the list that we are reading in
--- ids-server/server.js
+++ ids-server/server.js
@@ -1,22 +1,49 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const fs = require("node:fs");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-const expressWs = require('express-ws')(app);
+const expressWs = require("express-ws")(app);
 
-const loginIntrusionChannel = expressWs.getWss('intrusions');
-const intrusions = [];
+const loginIntrusionChannel = expressWs.getWss("intrusions");
+
+const IntrusionTypes = {
+  SqlInjection: "Sql Injection",
+  DOS: "Denial Of Service",
+};
+
+// read in the wordlist of sql injection strings
+const sqlInjectionStrings = new Set(
+  fs
+    //.readFileSync("./wordlists/wordlist.txt")
+    .readFileSync("wordlist.txt")
+    .toString("UTF8")
+    .split("\n")
+);
+
+const isSqlInjectionAttempt = (username, password) => {
+  return sqlInjectionStrings.has(username) || sqlInjectionStrings.has(password);
+};
 
 app.use(bodyParser.json());
-app.get('/test', (req, res) => {
-    res.send('server is running');
-})
-app.post('/login', (req, _) => {
-    // check the mirrored request for matches to known sql injection strings
-    console.log(req.body);
+app.get("/test", (req, res) => {
+  res.send("server is running");
+});
 
+app.post("/login", (req, _) => {
+  // check the mirrored request for matches to known sql injection strings
+  if (isSqlInjectionAttempt(req.body.username, req.body.password)) {
+    loginIntrusionChannel.clients.forEach((client) => {
+      client.send({
+        intrusionType: IntrusionTypes.SqlInjection,
+        info: `Attempted SQL injection with login parameters = ${
+          (req.body.username, req.body.password)
+        }.`,
+      });
+    });
+  }
 });
 
 app.listen(7000, () => {
-    console.log('ids server running on 6000');
+  console.log("ids server running on 7000");
 });
