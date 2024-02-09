15f7fdb4e7e8b692470058a6cd03ebe58ea53ae8
Mitchell Gannon <mgannon@flandersinc.com>
2024-02-09 10:46:50 +0800

    6000 was an unsafe port so it cannot be used

--- ids-server/server.js
+++ ids-server/server.js
@@ -7,6 +7,16 @@ const expressWs = require('express-ws')(app);
 const loginIntrusionChannel = expressWs.getWss('intrusions');
 const intrusions = [];
 
+app.use(bodyParser.json());
+app.get('/test', (req, res) => {
+    res.send('server is running');
+})
 app.post('/login', (req, _) => {
-    
+    // check the mirrored request for matches to known sql injection strings
+    console.log(req.body);
+
+});
+
+app.listen(7000, () => {
+    console.log('ids server running on 6000');
 });
