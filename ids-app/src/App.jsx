b90210f9c5d8302b8405a1e82c0ec2a283f115b6
Mitchell Gannon <mitchell.paul.gannon@gmail.com>
2024-02-15 08:03:52 +0800

    added the logo to the header

--- ids-app/src/App.jsx
+++ ids-app/src/App.jsx
@@ -3,17 +3,10 @@ import io from "socket.io-client";
 
 function App() {
   const [intrusionList, setIntrusionList] = useState([]);
-  const [isConnected, setIsConnected] = useState(false);
 
   useEffect(() => {
     // Initialize socket connection
     const socket = io("http://localhost", { path: "/ids-server/" });
-
-    // Setup event listeners
-    socket.on("connect", () => {
-      console.log("connected to server..");
-      setIsConnected(true);
-    });
 
     socket.on("intrusion-detected", (data) => {
       setIntrusionList(data);
@@ -27,11 +20,25 @@ function App() {
   }, []); // Empty dependency array means this effect runs only once on mount
 
   return (
-    <div>
-      {isConnected && <h1>Detected Intrusions</h1>}
-      {intrusionList.map((intrusion, index) => (
-        <p key={index}>{intrusion.info}</p>
-      ))}
+    <div className="grid gap-4">
+      {/* header */}
+      <div className="grid grid-cols-3 content-center h-20 px-4 shadow-md shadow-slate-200 bg-[#F5CF02]">
+        <img src="./unsw-logo.png" alt="" style={{ height: '80px' }} />
+        <h1>Intrusion Detection System</h1>
+      </div>
+
+      {/* intrusions list */}
+      <div className="rounded border">
+        {intrusionList.map((intrusion, index) => (
+          <p key={index}>{intrusion.info}</p>
+        ))}
+      </div>
+
+      <footer className="grid grid-cols-3 p-4">
+        <span>Mitchell Gannon</span>
+        <span className="place-self-center">z5433412</span>
+        <span className="place-self-end">Cyber Operations</span>
+      </footer>
     </div>
   );
 }
